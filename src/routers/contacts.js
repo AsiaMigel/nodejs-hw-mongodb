bfcb70c81f63866bf311e5aaea4402954b53f504
Asia <royfinaa@gmail.com>
2024-09-02 15:45:38 +0300

    fix server

---
+++ src/routers/contacts.js
@@ -0,0 +1,27 @@
+// import { Router } from 'express';
+// import { getAllContacts, getContactById } from '../services/contacts.js';
+import express from 'express';
+
+import {
+  getContactsController,
+  getContactByIdController,
+  createContactController,
+  deleteContactController,
+  patchContactController,
+} from '../controllers/contacts.js';
+
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const router = express.Router();
+
+router.get('/contacts', ctrlWrapper(getContactsController));
+
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactController));
+
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+
+export default router;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,17 +1,47 @@
 import { ContactCollection } from '../db/models/contact.js';
 
 export const getAllContacts = async () => {
-    try {
-        const contacts = await ContactCollection.find(); //повертає масив усіх контактів
-        console.log('Contacts:', contacts);
-        return contacts;
-    } catch (error) {
-        console.log('Error fetching contacts:', error);
-        throw error;
-    };
+  try {
+    const contacts = await ContactCollection.find(); //повертає масив усіх контактів
+    console.log('Contacts:', contacts);
+    return contacts;
+  } catch (error) {
+    console.error('Error fetching contacts:', error);
+    throw error;
+  }
 };
 
 export const getContactById = async (contactId) => {
-    const contact = await ContactCollection.findById(contactId);
-    return contact;
+  //повертає контакт за id
+  const contact = await ContactCollection.findById(contactId);
+  return contact;
+};
+
+//створення нового контакту
+export const createContact = async (payload) => {
+  const contact = await ContactCollection.create(payload);
+  return contact;
+};
+
+//зміна даних у контакті
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await ContactCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    { new: true, includeResultMetadata: true, ...options },
+  );
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
+//видалення контакту
+export const deleteContact = async (contactId) => {
+  const contact = await ContactCollection.findByIdAndDelete({
+    _id: contactId,
+  });
+  return contact;
 };
