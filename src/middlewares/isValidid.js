527bc137ce632dd11ec00a0eab9ace52f57a63b3
Asia <royfinaa@gmail.com>
2024-09-03 18:19:49 +0300

    fix isValidid

--- src/middlewares/isValidid.js
+++ src/middlewares/isValidid.js
@@ -1,10 +1,10 @@
 import createHttpError from 'http-errors';
 import { isValidObjectId } from 'mongoose';
 
-export const isValidid = (req, res, next) => {
+export const isValidId = (req, res, next) => {
   const { contactId } = req.params;
   if (!isValidObjectId(contactId)) {
-    throw createHttpError(400, 'It is not valid');
+    throw createHttpError(400, 'Id is not valid');
   }
   next();
 };
