3b996557254f44da892b79dd2918895d40d1a038
Asia <royfinaa@gmail.com>
2024-09-03 18:04:33 +0300

    update validation

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,19 +1,58 @@
+import { SORT_ORDER } from '../constants/index.js';
 import { ContactCollection } from '../db/models/contact.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async () => {
-  try {
-    const contacts = await ContactCollection.find(); //повертає масив усіх контактів
-    console.log('Contacts:', contacts);
-    return contacts;
-  } catch (error) {
-    console.error('Error fetching contacts:', error);
-    throw error;
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+  filter = {},
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = ContactCollection.find();
+
+  if (filter.contactType) {
+    contactsQuery.where('contactType').equals(filter.contactType);
   }
+
+  if (filter.isFavorite !== undefined) {
+    contactsQuery.where('isFavorite').equals(filter.isFavorite);
+  }
+
+  const [contactsCount, contacts] = await Promise.all([
+    ContactCollection.find().merge(contactsQuery).countDocuments(),
+    contactsQuery
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sortBy]: sortOrder })
+      .exec(),
+  ]);
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
+
+// export const getAllContacts = async ({page,perPage}) => {
+//   try {
+//     // const contacts = await ContactCollection.find();//повертає масив усіх контактів
+//     // console.log('Contacts:', contacts);
+//     // return contacts;
+//   } catch (error){
+//     console.error('Error fetching contacts:', error);
+//     throw error;
+//   };
+// };
 
 export const getContactById = async (contactId) => {
   //повертає контакт за id
-  const contact = await ContactCollection.findById(contactId);
+  const contact = await ContactCollection.findById({ _id: contactId });
   return contact;
 };
 
